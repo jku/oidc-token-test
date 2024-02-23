ac15ff708158848eb601d67a12632105bbe31ed5
Jussi Kukkonen <jku@goto.fi>
2024-02-23 15:29:40 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from sigstore.oidc import IdentityToken
 
-with open("tokendir/oidc-token.txt") as f:
+with open("./oidc-token.txt") as f:
     content = f.read().rstrip()
     token = IdentityToken(content)
     print("OK")
